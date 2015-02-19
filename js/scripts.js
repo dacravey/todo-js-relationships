61f5f4094bb27fe0360205aeb5072147022941c2
Debbie Cravey and Arsi <cinemaestro@yahoo.com>
2015-02-19 15:12:16 -0800

    add html and jquery to let user add list names.

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,14 @@ $(document).ready(function() {
   $("form#new-list").submit(function(event) {
     event.preventDefault();
 
-  var listName = $("input#list-name").val();
-    
+    var listName = $("input#list-name").val();
+    var newList = { tasks: [] };
+    newList.name = listName;
 
-  )};
+    $('ul#lists').append("<li><span class ='list-name'>" +
+    newList.name + "</span></li>");
 
+    $('input#new-list').val("");
+  });
 
-
-)};
+});
