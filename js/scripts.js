df63c6a5f57627c03e2812f80080aad841405b95
Debbie Cravey and Arsi <argoncode18@gmail.com>
2015-02-19 16:41:26 -0800

    update code to unbind click event listener and avoid duplicate task.

--- js/scripts.js
+++ js/scripts.js
@@ -12,24 +12,27 @@ $(document).ready(function() {
     $('input#new-list').val("");
 
     $(".list-name").last().click(function() {
+      $("ul#tasks").empty();
+
+      newList.tasks.forEach(function(task) {
+        $("ul#tasks").append("<li>" + task + "</li>");
+      });
+
       $("#show-list-name h2").text(newList.name);
       $("#show-list-name").show();
 
-    $("form#new-tasks").submit(function(event) {
-      event.preventDefault();
 
-      var newTask = $("input#new-task").val();
-      newList.tasks.push(newTask);
+      // unbind all other submit events on form#new-tasks using jQuery off method
+      $("form#new-tasks").off();
+      $("form#new-tasks").submit(function(event) {
+        event.preventDefault();
 
-      $("ul#tasks").append("<li>" + newList.tasks.slice(-1) + "</li>");
+        var newTask = $("input#new-task").val();
+        newList.tasks.push(newTask);
 
+        $("ul#tasks").append("<li>" + newTask + "</li>");
+      })
 
-    })
-
-
-
-
-
-  });
+    });
   });
 });
